02afd804f14cec8dde655938fa26c1fe73027c63
kevin hauman <kevin.hauman@zinderlabs.com>
2021-11-14 20:40:13 +0100

    moving issues resolved

--- src/components/map/routemap.vue
+++ src/components/map/routemap.vue
@@ -31,13 +31,14 @@
 export default {
   name: 'mapWrapper',
   data: () => ({
-      center: [0, 10],
+      center: [160, 10],
       projection: 'EPSG:4326',
       zoom: 4,
       rotation:0,
       radius:40,
       strokeWidth:10,
       strokeColor:'red',
+      moving_interval: null,
 
   }),
   props:{
@@ -48,10 +49,17 @@ export default {
   if(this.moving){
     this.moving_interval = setInterval(()=>{
       let [x,y]= this.center
-      this.center = [x+=0.1,y]
+      console.log(Math.round(x),y)
+      x = x>180? -180 : x+0.1
+      this.center = [x,y]      
       },16)
   }
+  },
+  beforeUnmount(){
+    if(this.moving_interval)
+    clearInterval(this.moving_interval)
   }
+
 };
 </script>
 
