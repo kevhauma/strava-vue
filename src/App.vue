7856df8c46bbc982d80365e214090c02ed22693b
Kevin Hauman <kevin.hauman@zinderlabs.com>
2021-11-14 15:11:13 +0100

    added vuesax

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,36 @@
 <template>
-  <div style="height:100%">    
+  <div style="height:100%">
+    <vs-navbar center-collapsed v-model="active">
+        <template #left>
+         Strava Map
+        </template>
+        <vs-navbar-item :active="active == 'guide'" id="guide">
+          Guide
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'docs'" id="docs">
+          Documents
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'components'" id="components">
+          Components
+        </vs-navbar-item>
+        <vs-navbar-item :active="active == 'license'" id="license">
+          license
+        </vs-navbar-item>
+        <template #right>
+          <vs-button flat >Login</vs-button>
+          <vs-button>Get Started</vs-button>
+        </template>
+      </vs-navbar>
     <router-view style="height:100%"/>
   </div>
 </template>
+  <script>
+  export default {
+    data:() => ({
+      active: 'guide'
+    })
+  }
+  </script>
 
 <style lang="scss">
 body {
--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router';
 import store from './store';
 import OpenLayersMap from 'vue3-openlayers';
 import 'vue3-openlayers/dist/vue3-openlayers.css';
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
 let app = createApp(App);
 app.use(store);
 app.use(router);
+app.use(Vuesax)
 app.use(OpenLayersMap);
 app.mount('#app');
